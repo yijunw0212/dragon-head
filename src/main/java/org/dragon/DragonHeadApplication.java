6569bab893aa40c75b4566ef1a2356ac329f29cd
acme <xiejun@192.168.1.9>
2026-03-12 21:10:37 +0800

    init

---
+++ src/main/java/org/dragon/DragonHeadApplication.java
@@ -0,0 +1,13 @@
+package org.dragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonHeadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DragonHeadApplication.class, args);
+    }
+
+}
